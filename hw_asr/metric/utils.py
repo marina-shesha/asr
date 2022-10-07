e517bd2d2f4c19826fe89da39b43565784e2bf56
marina sheshukova <sheshukovamarina06062001@gmail.com>
2022-10-07 19:55:11 +0300

    fix wer and cer

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -2,14 +2,14 @@
 import editdistance
 
 
-def calc_cer(target_text, predicted_text) -> float:
+def calc_wer(target_text, predicted_text) -> float:
     splited_target = target_text.split(' ')
     if len(splited_target) == 0:
         return 1
     return editdistance.distance(splited_target, predicted_text.split(' '))/len(splited_target)
 
 
-def calc_wer(target_text, predicted_text) -> float:
+def calc_cer(target_text, predicted_text) -> float:
     if len(target_text) == 0:
         return 1
     return editdistance.distance(target_text, predicted_text)/len(target_text)
